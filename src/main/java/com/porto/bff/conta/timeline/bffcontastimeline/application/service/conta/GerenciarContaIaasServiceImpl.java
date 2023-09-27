90c8ffb3b332a60923a2c68495ac79d0f59295f9
Landson Randel <P0657414@portoseguro.com.br>
2023-09-27 15:45:44 -0300

    validando os hedears

--- src/main/java/com/porto/bff/conta/timeline/bffcontastimeline/application/service/conta/GerenciarContaIaasServiceImpl.java
+++ src/main/java/com/porto/bff/conta/timeline/bffcontastimeline/application/service/conta/GerenciarContaIaasServiceImpl.java
@@ -2,8 +2,13 @@ package com.porto.bff.conta.timeline.bffcontastimeline.application.service.conta
 
 import com.porto.bff.conta.timeline.bffcontastimeline.infra.adapter.conta.ContaIassPortoAdapter;
 import com.porto.bff.conta.timeline.bffcontastimeline.presentation.rest.v1.contas.dto.*;
+import com.porto.bff.conta.timeline.bffcontastimeline.presentation.rest.v1.exception.TimelineIaasPortoException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 @Service
@@ -18,6 +23,7 @@ public class GerenciarContaIaasServiceImpl implements GerenciarContaIaasService{
             String xItauAuth,
             String contaId
     ) {
+        isValidHeader(xItauAuth, contaId);
         var respostaAdapter = adapter.getConta(
                 xItauAuth,
                 contaId
@@ -32,6 +38,7 @@ public class GerenciarContaIaasServiceImpl implements GerenciarContaIaasService{
             String xItauAuth,
             String contaId) {
 
+        isValidHeader(xItauAuth, contaId);
         var respostaAdapter = adapter.getContaSaldo(
                  xItauAuth,
                  contaId);
@@ -53,7 +60,7 @@ public class GerenciarContaIaasServiceImpl implements GerenciarContaIaasService{
                             String xItauAuth,
                             String contaId,
                             RequestDeleteDto request) {
-
+        isValidHeader(xItauAuth, contaId);
         adapter.apagarConta(xItauAuth, contaId, request);
 
     }
@@ -70,5 +77,18 @@ public class GerenciarContaIaasServiceImpl implements GerenciarContaIaasService{
                  requestDto
         );
 
+    }
+
+    private boolean isValidHeader(String xItauAuth, String contaId) {
+        List<TimelineIaasPortoException.TimelineIaasPortoErroItem> errors = new ArrayList<>();
+        try {
+            if(xItauAuth.isEmpty() || contaId.isEmpty()) {
+                throw new TimelineIaasPortoException("Campos obrigatorios", "407", errors);
+            } else {
+                return true;
+            }
+        }catch (Exception e) {
+            throw new TimelineIaasPortoException("Campos obrigatorios", "407", errors);
+        }
     }
 }
