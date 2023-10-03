a8f5c0cf701d4fdada6f6ee311fcef0de2919940
jeferson.silva <jeferson.silva@zup.com.br>
2023-10-03 10:06:16 -0300

    feat:Ajustes de modelagem

--- src/main/java/com/porto/bff/conta/timeline/bffcontastimeline/presentation/rest/v1/contas/dto/ContaBancariaDto.java
+++ src/main/java/com/porto/bff/conta/timeline/bffcontastimeline/presentation/rest/v1/contas/dto/ContaBancariaDto.java
@@ -1,5 +1,5 @@
 package com.porto.bff.conta.timeline.bffcontastimeline.presentation.rest.v1.contas.dto;
 
-public record ContaBancariaDto(String banco, String filial, String numero, String digitoDeVerificacao) {
+public record ContaBancariaDto(String banco, String agencia, String numero, String digitoConta) {
 }
 
