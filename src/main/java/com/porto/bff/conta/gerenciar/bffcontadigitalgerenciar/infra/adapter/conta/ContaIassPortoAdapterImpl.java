73355db22297140647d6229f5744f327a9db4084
Davisson Silva <dvvm.silva@gmail.com>
2023-10-18 11:54:39 -0300

    Add tratamento de erro padrão

--- src/main/java/com/porto/bff/conta/gerenciar/bffcontadigitalgerenciar/infra/adapter/conta/ContaIassPortoAdapterImpl.java
+++ src/main/java/com/porto/bff/conta/gerenciar/bffcontadigitalgerenciar/infra/adapter/conta/ContaIassPortoAdapterImpl.java
@@ -22,42 +22,21 @@ public class ContaIassPortoAdapterImpl implements ContaIassPortoAdapter {
 
     @Override
     public DataResponseIassPorto<AccountResponseIaasPorto> getConta(String xItauAuth, String contaId) {
-        try {
-            return this.client.findByIdContaIaas(this.getBearerInput(xItauAuth), "IAAS", contaId, contaId);
-        } catch (FeignException.FeignServerException | FeignException.FeignClientException exception) {
-            if (exception.status() == HttpStatus.UNAUTHORIZED.value()) {
-                throw new BusinessException(500, "IAAS_EXPIRATION_TOKEN", "AccessToken Inválido, gerar outro");
-            }
-            throw new BusinessException(exception.status(), "GET_CONTA_ERROR", exception.getLocalizedMessage());
-        }
+    	return this.client.findByIdContaIaas(this.getBearerInput(xItauAuth), "IAAS", contaId, contaId);
     }
 
     @Override
     public DataResponseIassPorto<BalanceResponseIaasPorto> getContaSaldo(String xItauAuth, String contaId) {
-        try {
-            return this.client.findBySaldoContaIaas(getBearerInput(xItauAuth), "IAAS", contaId, contaId);
-        } catch (FeignException.FeignServerException | FeignException.FeignClientException exception) {
-            if (exception.status() == HttpStatus.UNAUTHORIZED.value()) {
-                throw new BusinessException(500, "IAAS_EXPIRATION_TOKEN", "AccessToken Inválido, gerar outro");
-            }
-            throw new BusinessException(exception.status(), "GET_SALDO_CONTA_ERROR", exception.getLocalizedMessage());
-        }
+    	return this.client.findBySaldoContaIaas(getBearerInput(xItauAuth), "IAAS", contaId, contaId);
     }
 
     @Override
     public DataResponseIassPorto<SumarioResponseIaasPorto> sumarioConta(String tokenCognito, String xItauAuth, String contaId) {
-        try {
             DataResponseIassPorto<AccountResponseIaasPorto> dadosConta =
                     this.client.findByIdContaIaas(this.getBearerInput(xItauAuth), "IAAS", contaId, contaId);
             DataResponseIassPorto<BalanceResponseIaasPorto> saldoConta =
                     this.client.findBySaldoContaIaas(getBearerInput(xItauAuth), "IAAS", contaId, contaId);
             return new DataResponseIassPorto<>(new SumarioResponseIaasPorto(this.decodificador.getCpfPorToken(tokenCognito), dadosConta, saldoConta));
-        } catch (FeignException.FeignServerException | FeignException.FeignClientException exception) {
-            if (exception.status() == HttpStatus.UNAUTHORIZED.value()) {
-                throw new BusinessException(500, "IAAS_EXPIRATION_TOKEN", "AccessToken Inválido, gerar outro");
-            }
-            throw new BusinessException(exception.status(), "GET_SUMARIO_CONTA_ERROR", exception.getLocalizedMessage());
-        }
     }
 
     private String getBearerInput(String xItauAuth) {
