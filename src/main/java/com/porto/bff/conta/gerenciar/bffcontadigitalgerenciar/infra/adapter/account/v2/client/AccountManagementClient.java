ef2c3854cc16af941f84468c0a650b9b9c12ff16
jeferson.silva <jeferson.silva@zup.com.br>
2024-04-17 18:24:03 -0300

    fix: remove data response

--- src/main/java/com/porto/bff/conta/gerenciar/bffcontadigitalgerenciar/infra/adapter/account/v2/client/AccountManagementClient.java
+++ src/main/java/com/porto/bff/conta/gerenciar/bffcontadigitalgerenciar/infra/adapter/account/v2/client/AccountManagementClient.java
@@ -2,7 +2,6 @@ package com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.infra.adapter.acc
 
 
 import com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.common.utils.v2.HttpUtils;
-import com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.domain.model.BackendResponseData;
 import com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.domain.model.account.balance.v2.AccountBalanceEntityResponse;
 import com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.domain.model.account.data.v2.AccountDataEntityResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -16,15 +15,15 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @FeignClient(value = "AccountManagementPortoClient", url = "${feign.client.config.porto.host}")
 public interface AccountManagementClient {
     @GetMapping("${feign.client.config.rotes.backend.balance.endpoint}")
-    BackendResponseData<AccountBalanceEntityResponse> getBalanceAccount(@RequestHeader(AUTHORIZATION) String xItauAuth,
-                                                                        @RequestHeader(HttpUtils.HTTP_PROVIDER_HEADER) String xAccountProvider,
-                                                                        @RequestHeader(HttpUtils.HTTP_ACCOUNT_ID_HEADER) String xAccountId,
-                                                                        @PathVariable(HttpUtils.HTTP_ACCOUNT_ID_PATH_VARIABLE) String accountId);
+    AccountBalanceEntityResponse getBalanceAccount(@RequestHeader(AUTHORIZATION) String xItauAuth,
+                                                   @RequestHeader(HttpUtils.HTTP_PROVIDER_HEADER) String xAccountProvider,
+                                                   @RequestHeader(HttpUtils.HTTP_ACCOUNT_ID_HEADER) String xAccountId,
+                                                   @PathVariable(HttpUtils.HTTP_ACCOUNT_ID_PATH_VARIABLE) String accountId);
 
     @GetMapping("${feign.client.config.rotes.backend.account.endpoint}")
-    BackendResponseData<AccountDataEntityResponse> getAccountData(@RequestHeader(AUTHORIZATION) String xItauAuth,
-                                                                  @RequestHeader(HttpUtils.HTTP_PROVIDER_HEADER) String xAccountProvider,
-                                                                  @RequestHeader(HttpUtils.HTTP_ACCOUNT_ID_HEADER) String xAccountId,
-                                                                  @PathVariable(HttpUtils.HTTP_ACCOUNT_ID_PATH_VARIABLE) String accountId,
-                                                                  @RequestParam(HttpUtils.HTTP_ACCOUNT_FIELDS_PARAM) String fields);
+    AccountDataEntityResponse getAccountData(@RequestHeader(AUTHORIZATION) String xItauAuth,
+                                             @RequestHeader(HttpUtils.HTTP_PROVIDER_HEADER) String xAccountProvider,
+                                             @RequestHeader(HttpUtils.HTTP_ACCOUNT_ID_HEADER) String xAccountId,
+                                             @PathVariable(HttpUtils.HTTP_ACCOUNT_ID_PATH_VARIABLE) String accountId,
+                                             @RequestParam(HttpUtils.HTTP_ACCOUNT_FIELDS_PARAM) String fields);
 }
