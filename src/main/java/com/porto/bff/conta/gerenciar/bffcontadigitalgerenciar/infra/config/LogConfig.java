681bf8d605bfd3da676eb9691eb92d6b03c68248
jeferson.silva <jeferson.silva@zup.com.br>
2024-05-10 18:23:13 -0300

    log: fix

---
+++ src/main/java/com/porto/bff/conta/gerenciar/bffcontadigitalgerenciar/infra/config/LogConfig.java
@@ -0,0 +1,19 @@
+package com.porto.bff.conta.gerenciar.bffcontadigitalgerenciar.infra.config;
+
+import lombok.experimental.UtilityClass;
+import org.slf4j.MDC;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+@UtilityClass
+public class LogConfig {
+
+    public static <U> Supplier<U> withMdc(Supplier<U> supplier) {
+        Map<String, String> mdc = MDC.getCopyOfContextMap();
+        return () -> {
+            MDC.setContextMap(mdc);
+            return supplier.get();
+        };
+    }
+}
